a1b15f182059879a49d7887608965f392fdc6327
Elwin Ung <elwin.ung@gmail.com>
2020-11-17 16:38:44 -0500

    Changed placeholder text

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -4,9 +4,9 @@ function Searchbar(props){
     return (
         <>
             <div className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Keywoards" onChange={props.handleInputChange} value={props.search}/>
+                <input type="text" className="form-control" placeholder="Looking for someone?" onChange={props.handleInputChange} value={props.search}/>
                 <div className="input-group-append">
-                    <button className="btn btn-primary" type="button" onClick={props.handleFormSubmit}>Search</button>
+                    <button className="btn btn-primary" type="button" onClick={props.handleFormSubmit}>Filter</button>
                 </div>
             </div>
         </>
